1c7f602562e47e76d7cc900923a87a88d2644ac0
github username <github email address>
2017-11-02 16:08:43 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,18 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :sessions_secret, "fwitter_secret"
+  end
+
+  helpers do
+    def logged_in?
+      session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
   get '/' do
@@ -29,9 +41,43 @@ class ApplicationController < Sinatra::Base
     erb :users
   end
 
-  post '/sign-up' do 
+  post '/sign-up' do
     @user = User.new(:username => params[:username], :email => params[:email])
     @user.save
     redirect '/'
+  end
+
+  get '/login' do
+    erb :login
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user
+      session[:user_id] = user.id
+      redirect "/"
+    else
+      redirect '/users'
+    end
+  end
+
+  get '/tweet' do
+    if session[:user_id]
+      erb :tweet
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweet' do
+    user = User.find_by(:user_id => session[:user_id])
+    tweet = Tweet.new(:user => user, :status => params[:status])
+    tweet.save
+    redirect '/'
+  end
+
+  get '/logout' do
+    session.destroy
+    redirect '/login'
   end
 end
