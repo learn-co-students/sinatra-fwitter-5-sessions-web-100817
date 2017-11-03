b507446986f216b3265c900c3d9a762edda9f5ca
yassimortensen <yassimortensen@gmail.com>
2017-11-03 12:00:34 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,23 @@
 require './config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "fwitter_secret"
+  end
+
+  helpers do
+    def logged_in?
+      session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
   get '/' do
@@ -13,12 +26,30 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/login' do
+    erb :login
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user #is true
+      session[:user_id] = user.id
+      redirect '/'
+    else
+      redirect "/signup"
+    end
+  end
+
   get '/tweet' do
-    erb :tweet
+    if session[:user_id] #returns nil if there isn't one
+      erb :tweet
+    else
+      redirect '/login'
+    end
   end
 
   post '/tweet' do
-    user = User.find_by(:username => params[:username])
+    user = User.find_by(user_id: session[:user_id])
     tweet = Tweet.new(:user => user, :status => params[:status])
     tweet.save
     redirect '/'
@@ -29,9 +60,14 @@ class ApplicationController < Sinatra::Base
     erb :users
   end
 
-  post '/sign-up' do 
+  post '/sign-up' do
     @user = User.new(:username => params[:username], :email => params[:email])
     @user.save
     redirect '/'
+  end
+
+  get '/logout' do
+    session.destroy
+    redirect '/login'
   end
 end
